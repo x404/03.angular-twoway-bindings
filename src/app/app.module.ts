0d134d58f0dc85f6110d0acaabb0d4c4b6744921
x404 <kronos2003@gmail.com>
2024-04-19 18:35:18 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet, Routes } from "@angular/router";
+import { NgModelComponent } from './ng-model/ng-model.component';
+import { FormsModule } from "@angular/forms";
+
+const appRoutes: Routes = [
+  { path: 'ngmodel', component: NgModelComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgModelComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    RouterLink,
+    RouterLinkActive,
+    RouterLink,
+    RouterLinkActive,
+    RouterLink,
+    RouterLinkActive,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
